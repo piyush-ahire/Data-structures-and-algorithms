7ff5e96848f9c72f55704465b314c7ba488e33a8
piyush-ahire <piyushahire402@gmail.com>
2024-01-12 20:39:46 +0530

    leetcode and codestudio questions added

---
+++ STL/reverseArr.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+vector<int> reverse(vector<int>v){
+    int s = 0, e = v.size() - 1;
+
+    while (s<=e)
+    {
+        swap(v[s], v[e]);
+        s++;
+        e--;
+    }
+
+    return v;
+    
+}
+
+void print(vector<int> v){
+    for(int i:v){
+        cout<<i<<" ";
+    }
+}
+int main(){
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(6);
+    
+
+    vector<int> ans = reverse(v);
+    cout<<"Printing the reverse of arr : ";
+    print(ans);
+    return 0;
+}
