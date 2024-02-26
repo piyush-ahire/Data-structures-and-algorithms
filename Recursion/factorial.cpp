26a060f86205e603afdabe23f5fd37d3d1c99a4c
piyush-ahire <piyushahire402@gmail.com>
2024-02-26 15:49:57 +0530

    Recursion starts

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    //base case - is always mandatory , otherwise segmentation fault occurs
+    if(n==0)
+        return 1;
+
+    //small problem
+    //int small = factorial(n-1);
+
+    //big problem
+    //int big = n * small;
+
+    return n * factorial(n-1);
+}
+
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int ans = factorial(n);
+
+    cout<<ans<<endl;
+
+    return 0;
+}
