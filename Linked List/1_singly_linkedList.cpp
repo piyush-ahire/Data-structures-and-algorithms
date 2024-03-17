8b2f248a2c95d2bda29e28850c74a3d537f5a3f7
piyush-ahire <piyushahire402@gmail.com>
2024-03-17 20:01:12 +0530

    Done with singly linked list

--- Linked List/1_singly_linkedList.cpp
+++ Linked List/1_singly_linkedList.cpp
@@ -14,6 +14,17 @@ class Node{
         this->data = data;
         this->next = NULL;
     }
+
+    //Destructor to free up memory after deletion
+    ~Node(){
+        int val = this->data;
+        //free up memory
+        if(this->next != NULL){
+            delete next;
+            this->next = NULL;
+        }
+        cout<<"Memory is free for node with data "<<val<<endl;
+    }
 };
 
 
@@ -71,8 +82,48 @@ void InsertInBetween(Node* &tail, Node* &head, int pos, int d){
 
     temp->next = nodeToInsert;
 
-
 }
+
+
+//Deletion of node in linked list
+void deleteNode(int pos, Node* &head){
+
+    //deleting the first node
+    if(pos == 1){
+        Node* temp = head;
+        head = head->next;
+
+        //freeup memory
+        temp->next = NULL;
+
+        delete temp;
+    }
+    else{
+
+        //deleting any middle node or last node
+        Node* curr = head;
+        Node* prev = NULL;
+
+        int cnt = 1;
+
+        while(cnt<pos){
+            prev = curr;
+
+            curr = curr->next;
+            cnt++;
+        }
+
+        prev->next = curr->next;
+
+        curr->next = NULL;
+        delete curr;
+    }
+    
+}
+
+
+
+
 
 
 //Traverse a linked list
@@ -147,8 +198,18 @@ int main(){
     print(head);
     cout<<endl;
 
-     cout<<"Head is "<<head->data<<endl;
+    cout<<"Head is "<<head->data<<endl;
     cout<<"Tail is "<<tail->data<<endl;
+    cout<<endl;
+
+
+    //Deleting the nodes in linked list
+    deleteNode(1, head);
+    print(head);
+    cout<<endl;
+
+    deleteNode(4, head);
+    print(head);
     cout<<endl;
 
     return 0;
