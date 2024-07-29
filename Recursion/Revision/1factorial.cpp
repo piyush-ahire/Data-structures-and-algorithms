e3d29f79cbbdcf07bf3db004e330fc5753e9e5c1
piyush-ahire <piyushahire402@gmail.com>
2024-07-29 21:17:16 +0530

    Recursion Day-1

---
+++ Recursion/Revision/1factorial.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+
+    //base case
+    if(n == 0)
+        return 1;
+
+    int small = factorial(n-1);
+    int large = n * small;
+
+    return large;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int ans = factorial(n);
+
+    cout<<ans<<endl;
+
+    return 0;
+}
